510be85c71d2472676a14903a29df05b33ed971a
danilo.volles <danilo.volles@outlook.com>
2024-06-21 21:17:30 -0300

    rfct: simplificação de código com o modelMapper.

--- src/main/kotlin/danilovolles/productms/service/ProductServiceImpl.java
+++ src/main/kotlin/danilovolles/productms/service/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package danilovolles.productms.service;
 import danilovolles.productms.dto.ProductDTO;
 import danilovolles.productms.model.Product;
 import danilovolles.productms.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,26 +17,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Optional<ProductDTO> create(ProductDTO request) {
-
-        Product product = new Product();
-
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
-        product.setDescription(request.getDescription());
-        product.setAvailable(request.isAvailable());
-
+        ModelMapper modelMapper = new ModelMapper();
+        Product product = modelMapper.map(request, Product.class);
         productRepository.saveAndFlush(product);
-
-//        /* Opcional, apenas para não retornar uma request, que fica feio: */
-//        ProductDTO response = new ProductDTO();
-//
-//        response.setName((product.getName()));
-//        response.setPrice(product.getPrice());
-//        response.setDescription(request.getDescription());
-//        response.setAvailable(request.isAvailable());
-//
-//        return Optional.of(response);
-
-        return Optional.of(request);
+        ProductDTO response = modelMapper.map(product, ProductDTO.class);
+        return Optional.of(response);
     }
 }
