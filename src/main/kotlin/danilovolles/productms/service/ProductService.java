988a375049c52cdaed0c21d283293c670a591e02
danilo.volles <danilo.volles@outlook.com>
2024-06-21 21:35:59 -0300

    feat: ProductService.getAll()

--- src/main/kotlin/danilovolles/productms/service/ProductService.java
+++ src/main/kotlin/danilovolles/productms/service/ProductService.java
@@ -3,9 +3,11 @@ package danilovolles.productms.service;
 import danilovolles.productms.dto.ProductDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public interface ProductService {
     Optional<ProductDTO> create(ProductDTO request);
+    List<ProductDTO> getAllProducts();
 }
--- src/main/kotlin/danilovolles/productms/service/ProductServiceImpl.java
+++ src/main/kotlin/danilovolles/productms/service/ProductServiceImpl.java
@@ -7,20 +7,41 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private ModelMapper modelMapper;
 
     @Autowired
     private ProductRepository productRepository;
 
     @Override
     public Optional<ProductDTO> create(ProductDTO request) {
-        ModelMapper modelMapper = new ModelMapper();
+
         Product product = modelMapper.map(request, Product.class);
         productRepository.saveAndFlush(product);
         ProductDTO response = modelMapper.map(product, ProductDTO.class);
         return Optional.of(response);
+
     }
+
+    @Override
+    public List<ProductDTO> getAllProducts() {
+
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> responses = new ArrayList<>();
+
+        products.forEach(product -> {
+            ProductDTO response = modelMapper.map(product, ProductDTO.class);
+            responses.add(response);
+        });
+
+        return responses;
+    }
+
+
 }
